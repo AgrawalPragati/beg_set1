b159b7165254f3241ad3eb6fe1972e2b7196b7a9
AgrawalPragati <35077511+AgrawalPragati@users.noreply.github.com>
2018-01-14 23:44:10 +0530

    Add files via upload

---
+++ eveod.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{int n;
+	printf("Enter a no: ");
+    scanf("%d",&n);
+    if(n%2==0)
+     printf("Number is even.");
+     else
+     printf("Number is odd.");
+ }
